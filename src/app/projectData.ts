3640e7d655c1d6428c33900ba7bef0d15c782294
joshuak08 <joshuakongxw@gmail.com>
2024-02-03 21:07:11 +0000

    remove unfinished projects data

--- src/app/projectData.ts
+++ src/app/projectData.ts
@@ -1,31 +1,31 @@
-import { ProjectCardProps } from "./types";
+// import { ProjectCardProps } from "./types";
 
-export const projectData: ProjectCardProps[] = [
-    {
-        title: "Project 1",
-        year: 2021,
-        description: "This is a project description.",
-        image: "https://images.unsplash.com/photo-1612833606943-5a1e9eac7e8d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-        link: "https://www.google.com",
-        technologies: ["React", "Next.js", "Tailwind CSS"],
-        highlights: ["This is a highlight", "This is another highlight"]
-    },
-    {
-        title: "Project 2",
-        year: 2021,
-        description: "This is a project description.",
-        image: "https://images.unsplash.com/photo-1612833606943-5a1e9eac7e8d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-        link: "https://www.google.com",
-        technologies: ["React", "Next.js", "Tailwind CSS"],
-        highlights: ["This is a highlight", "This is another highlight"]
-    },
-    {
-        title: "Project 3",
-        year: 2021,
-        description: "This is a project description.",
-        image: "https://images.unsplash.com/photo-1612833606943-5a1e9eac7e8d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-        link: "https://www.google.com",
-        technologies: ["React", "Next.js", "Tailwind CSS"],
-        highlights: ["This is a highlight", "This is another highlight"]
-    },
-]
+// export const projectData: ProjectCardProps[] = [
+//     {
+//         title: "Project 1",
+//         year: 2021,
+//         description: "This is a project description.",
+//         image: "https://images.unsplash.com/photo-1612833606943-5a1e9eac7e8d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+//         link: "https://www.google.com",
+//         technologies: ["React", "Next.js", "Tailwind CSS"],
+//         highlights: ["This is a highlight", "This is another highlight"]
+//     },
+//     {
+//         title: "Project 2",
+//         year: 2021,
+//         description: "This is a project description.",
+//         image: "https://images.unsplash.com/photo-1612833606943-5a1e9eac7e8d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+//         link: "https://www.google.com",
+//         technologies: ["React", "Next.js", "Tailwind CSS"],
+//         highlights: ["This is a highlight", "This is another highlight"]
+//     },
+//     {
+//         title: "Project 3",
+//         year: 2021,
+//         description: "This is a project description.",
+//         image: "https://images.unsplash.com/photo-1612833606943-5a1e9eac7e8d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+//         link: "https://www.google.com",
+//         technologies: ["React", "Next.js", "Tailwind CSS"],
+//         highlights: ["This is a highlight", "This is another highlight"]
+//     },
+// ]
