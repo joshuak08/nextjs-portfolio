30babeb18ca452ce8d408b33d83ce082b4c8e1cd
joshuak08 <joshuakongxw@gmail.com>
2024-02-11 23:55:27 +0000

    revamped foooter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,19 @@
 export default function Footer(){
-    return (<footer className="mt-12 pageLanding">footer goes here asdf </footer>)
+    return (
+        // add logos and made by statements
+        <footer className="mt-12 pageLanding flex">
+            <p>
+                Let&#39;s get in touch! Connect with me on&nbsp;
+                <span>
+                    <a className="underline decoration-gray-600 hover:decoration-gray-200 transition-all ease-in-out" target="_blank" href="https://www.linkedin.com/in/joshua-kong-xw">LinkedIn</a> ↗
+                </span>
+                &nbsp;or&nbsp;
+                <span>
+                    <a className="underline decoration-gray-600 hover:decoration-gray-200 transition-all ease-in-out" target="_blank" href="mailto:joshuaxwkong@gmail.com">joshuaxwkong@gmail.com</a> 
+                </span>
+                .
+            </p>
+
+        </footer>
+    )
 }
