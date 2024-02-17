e15a017762a519d9a7f06c73e27e007c56e81c6a
joshuak08 <joshuakongxw@gmail.com>
2024-02-17 00:02:19 +0000

    added typing animations, still needs styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 "use client"
+import { TypeAnimation } from 'react-type-animation';
+
 export default function Home() {
     return (
         <>
             {/* typewriter animation here for all fields */}
-            <h1>Hey there! I&#39;m Joshua Kong. </h1>
+            <TypeAnimation
+                sequence={[
+                    "Hey there! I'm Joshua Kong.",
+                    1500,
+                    "I'm a final year Computer Science student at the University of Bristol.",
+                    1500,
+                    "I'm an aspiring Software Engineer currently looking for any junior/graduate roles.",
+                    1500,
+                    "Feel free to explore my portfolio and get in touch with me!",
+                ]}
+                wrapper="h1"
+                speed={50}
+                deletionSpeed={75}
+            />
+            {/* <h1>Hey there! I&#39;m Joshua Kong. </h1> */}
             {/* include pic here maybe */}
             <p>Previously at TD Securities London, UK
             </p>
